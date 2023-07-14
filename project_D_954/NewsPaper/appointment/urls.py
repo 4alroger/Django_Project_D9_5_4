3b22c8249d11d28e455bcb7fa8a832fd0f1f9d85
4alroger <129160214+4alroger@users.noreply.github.com>
2023-07-14 20:50:44 +0400

    Start
    Start

---
+++ project_D_954/NewsPaper/appointment/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+#from NewsPaper.appointment.views import AppointmentView
+
+
+urlpatterns = [
+    #path('', AppointmentView.as_view(), name='appointments'),
+
+]
